a488112442f925b3bb90e6d4ac7270b742081ba1
mwaqas1122 <56536965+mwaqas1122@users.noreply.github.com>
2019-12-10 16:23:24 +0200

    Add files via upload

---
+++ Part1.ps1
@@ -0,0 +1,28 @@
+﻿Connect-AzureRmAccount
+
+Get-AzLocation | select Location
+$location = "eastus"
+
+$resourceGroup = "myResourceGroup"
+New-AzResourceGroup -Name $resourceGroup -Location $location
+
+
+$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroup `
+  -Name "blogarticles" `
+  -SkuName Standard_LRS `
+  -Location $location `
+
+$ctx = $storageAccount.Context
+
+
+
+
+
+$containerName = "quickstartblobs"
+New-AzStorageContainer -Name $containerName -Context $ctx -Permission blob
+
+
+Set-AzStorageBlobContent -File "C:\DevOpsTask2\text1.txt" `
+  -Container $containerName `
+  -Blob "text1.txt" `
+  -Context $ctx 
